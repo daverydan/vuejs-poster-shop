253aed69ae8de7ae7a904f30cd49cfc0b7c59e44
Danny Avery <danny.w.avery@gmail.com>
2018-04-17 20:11:32 -0600

    Prevent duplicate cart items + create currency filter

--- public/script.js
+++ public/script.js
@@ -1,21 +1,41 @@
+var PRICE = 9.99;
 new Vue({
 	el: '#app',
 	data: {
 		total: 0,
 		items: [
-			{ title: 'Item 1' },
-			{ title: 'Item 2' },
-			{ title: 'Item 3' }
+			{ id: 1, title: 'Item 1' },
+			{ id: 2, title: 'Item 2' },
+			{ id: 3, title: 'Item 3' }
 		],
 		cart: []
 	},
 	methods: {
 		addItem: function(index) {
-			// console.log(index);
-			this.total += 9.99;
-			this.cart.push(this.items[index]);
-			// check if items are going into cart
-			// console.log(this.cart.length);
+			this.total += PRICE; // increment item total
+			var item = this.items[index]; // find the item by index
+			var found = false; // init found
+			// check if item found in cart
+			for (var i = 0; i < this.cart.length; i++) {
+				if (this.cart[i].id === item.id) {
+					found = true; // item found in cart
+					this.cart[i].qty++;
+				}
+			}
+			// if item not found in cart - put in cart
+			if (!found) {
+				this.cart.push({
+					id: item.id,
+					title: item.title,
+					qty: 1,
+					price: PRICE
+				});
+			}
+		}
+	},
+	filters: {
+		currency: function(price) {
+			return '$'.concat(price.toFixed(2));
 		}
 	}
 });
